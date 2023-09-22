7c4acd636e8cc8f78a7c0a5bd373c9f736dc9b11
bzvol <zvolenszki.benjamin@gmail.com>
2023-09-22 11:26:18 +0200

    add create map reduce file action

---
+++ src/main/kotlin/me/bzvol/hadoopplugin/CreateJavaMapReduceFileAction.kt
@@ -0,0 +1,22 @@
+package me.bzvol.hadoopplugin
+
+import com.intellij.ide.actions.CreateFileFromTemplateAction
+import com.intellij.ide.actions.CreateFileFromTemplateDialog
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDirectory
+import com.intellij.util.PlatformIcons.CLASS_ICON
+
+class CreateJavaMapReduceFileAction :
+    CreateFileFromTemplateAction("MapReduce File (Java)", "Create a new MapReduce file with Java", CLASS_ICON) {
+    override fun buildDialog(project: Project, directory: PsiDirectory, builder: CreateFileFromTemplateDialog.Builder) {
+        builder
+            .setTitle("New MapReduce File (Java)")
+            .addKind("Mapper", CLASS_ICON, "MapReduce Mapper")
+            .addKind("Reducer", CLASS_ICON, "MapReduce Reducer")
+            .addKind("Driver", CLASS_ICON, "MapReduce Driver")
+    }
+
+    override fun getActionName(directory: PsiDirectory, newName: String, templateName: String): String {
+        return "Create Java MapReduce File: $newName"
+    }
+}
